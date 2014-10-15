e88167ba2c9927cf168ec454e5ae498686cf9e8b
Lev Rechnik <relevv@gmail.com>
2014-10-15 17:19:04 +0400

    Update make brew

--- backuper.rb
+++ backuper.rb
@@ -12,6 +12,7 @@ class Backuper < Formula
 
   def install
     distdir = 'src/github.com/headmade/backuper'
+    print "here"
     print system "ls -lR #{Dir.pwd}"
     print system "mkdir -p _#{distdir} && mv [a-z]* _#{distdir} && mv _src src"
     print system "ls -lR #{Dir.pwd}"
