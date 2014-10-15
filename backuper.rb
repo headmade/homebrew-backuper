91d1f8c0b65bcc0d8223137d857c4d95dffa17c7
Lev Rechnik <relevv@gmail.com>
2014-10-15 17:12:33 +0400

    Update make brew

--- backuper.rb
+++ backuper.rb
@@ -12,8 +12,9 @@ class Backuper < Formula
 
   def install
     distdir = 'src/github.com/headmade/backuper'
+    print system 'ls -lR #{Dir.pwd}'
     print system "mkdir -p _#{distdir} && mv [a-z]* _#{distdir} && mv _src src"
-    print system 'ls -lR'
+    print system 'ls -lR #{Dir.pwd}'
     print system "cd #{distdir} && ls -l && GOPATH=#{Dir.pwd} make brew"
     exit
     bin.install "#{distdir}/gobackuper"
